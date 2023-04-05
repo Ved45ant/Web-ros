2d42fb96e44e0d556d06358b0993abc806249d21
Utkarsh0255 <utkarshpandey.net@gmail.com>
2023-04-05 12:38:07 +0530

    portfolio finished

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const textColor=["black","#f7bea9","white","purple","white","violet","aqua"]
-const backg = ["bg7","bg1","bg2","bg3","bg4","bg5","bg6"]
+const textColor=["white","#aqua","purple","#ffd700","#000080","aqua"]
+const backg = ["bg1","bg2","bg3","bg4","bg5","bg6"]
 let x=0;
 
 const anchor = document.getElementsByTagName("a");
@@ -22,19 +22,57 @@ function myFunction() {
     if(x==textColor.length){
         x=0
     }
-    console.log(x)
+    // console.log(x)
    },4000
 )
 // window.onclick=myFunction;
 
 // active
 
-for (var i = 0; i < anchor.length; i++) {
-    anchor[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
+// for (var i = 0; i < anchor.length; i++) {
+//     anchor[i].addEventListener("click", function() {
+//       var current = document.getElementsByClassName("active");
+//       current[0].className = current[0].className.replace(" active", "");
+//       this.className += " active";
+//     });
 
-}  
+// }  
 
+
+
+const bars=document.getElementById("bars");
+
+function nav(){
+    document.getElementsByClassName("nav-ul")[0].classList.toggle('mobileview');
+    // document.getElementsByClassName("nav-ul")[0].style.display='block' 
+}
+bars.onclick=nav
+
+
+
+const cards=document.querySelectorAll(".card")
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+    entry.target.classList.toggle("show", entry.isIntersecting)
+
+})
+})
+cards.forEach(card => {
+    observer.observe(card)
+})
+
+const d=new Date()
+const hour=d.getHours()
+if (hour>5&&hour<12) {
+    document.getElementById("greet").innerHTML='Good Morning,'
+}
+else if (hour>=12&&hour<17) {
+    document.getElementById("greet").innerHTML='Good Afternoon,'
+}
+else if (hour>=17&&hour<22) {
+    document.getElementById("greet").innerHTML='Good Evening,'
+}
+else{
+    document.getElementById("greet").innerHTML='HEY,'
+    
+}
